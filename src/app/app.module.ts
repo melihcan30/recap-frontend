9abc76d0dc85a9d66b30009eacf69efc309f0fe9
acer <acer@DESKTOP-QDHOQ5E>
2021-03-16 20:51:36 +0300

    16 Mart commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
--- src/app/components/rental/rental.component.ts
+++ src/app/components/rental/rental.component.ts
@@ -25,5 +25,4 @@ export class RentalComponent implements OnInit {
       console.log(this.rentals);
     })
   }
-
 }
