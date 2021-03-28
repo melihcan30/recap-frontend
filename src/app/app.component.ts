5f2791df6ee86e2c6eb83a6d80e18db7b7475531
acer <acer@DESKTOP-QDHOQ5E>
2021-03-28 17:46:01 +0300

    28 Mart Commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [NgbCarouselConfig]
 })
-
 export class AppComponent {
-  title:string = 'recap';
-  user:string = 'Melihcan Serbest';
+  title = 'recap';
+
 }
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {LocalStorageService} from '../../services/local-storage.service';
+import {UserService} from '../../services/user.service';
+import {ToastrService} from 'ngx-toastr';
+import {Router} from '@angular/router';
+import {User} from '../../models/user';
 
 @Component({
   selector: 'app-navi',
-  templateUrl: './navi.component.html',
-  styleUrls: ['./navi.component.css']
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
 })
 export class NaviComponent implements OnInit {
+  email = this.localStorageService.get('email');
+  user:User=new User();
 
-  constructor() { }
+  constructor(private authService:AuthService,private localStorageService:LocalStorageService,private userService:UserService,private  toastrService:ToastrService,private router:Router) { }
 
   ngOnInit(): void {
+    this.checkToLogin();
+    this.checkToEmail();
+    this.getEmail();
   }
 
+  checkToLogin(){
+    if(this.authService.isAuthenticated()){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  checkToEmail(){
+    if(this.localStorageService.get('email')){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  logOut(){
+   this.localStorageService.clean()
+    this.toastrService.success("Başarıyla Çıkış Yapıldı");
+    this.router.navigate(["/"])
+  }
+
+  getEmail(){
+    if(this.email){
+      this.userService.getByEmail(this.email).subscribe(response=>{
+        this.user = response;
+      })
+    }
+  }
 }
